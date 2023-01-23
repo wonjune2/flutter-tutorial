dfbc1c5d17cc1bf5bdd2dd8bd6f95b19e0410de7
Joon <zzzdnjswns@naver.com>
2023-01-23 20:26:26 +0900

    tutorial

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,52 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcom to Flutter'),
-          ),
-          body: Row(
-            children: const [
-              Expanded(
-                child: FittedBox(
-                  child: Text('Data'),
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: BlueBox(),
-              ),
-              Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: BlueBox(),
-              )
-            ],
-          )),
-    );
-  }
-}
-
-class BlueBox extends StatelessWidget {
-  const BlueBox({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        border: Border.all(),
-      ),
-    );
-  }
+  runApp(const Center(
+    child: Text(
+      'Hello World!',
+      textDirection: TextDirection.ltr,
+    ),
+  ));
 }
