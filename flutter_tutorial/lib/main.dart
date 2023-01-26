001a8a887e638419df7712c1448c67bdcf17061e
Joon <zzzdnjswns@naver.com>
2023-01-26 23:18:20 +0900

    stateful

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -107,3 +107,53 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class FaboriteWidget extends StatefulWidget {
+  const FaboriteWidget({super.key});
+
+  @override
+  State<FaboriteWidget> createState() => _FaboriteWidgetState();
+}
+
+class _FaboriteWidgetState extends State<FaboriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            onPressed: _toggleFavorite,
+            icon: _isFavorited
+                ? const Icon(Icons.start)
+                : const Icon(Icons.star_border),
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+}
