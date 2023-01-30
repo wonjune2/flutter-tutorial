a266accf0d7d4e77b42b51b3bfba681c31451c85
Joon <zzzdnjswns@naver.com>
2023-01-30 22:52:54 +0900

    layout review

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,156 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(const MyApp());
+void main() {
+  debugPaintSizeEnabled = true; // Remove to suppress visual layout
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campaground',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                )
-              ],
-            ),
-          ),
-          const FavoriteWeget(),
-        ],
-      ),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter layout demo'),
-          ),
-          body: ListView(
-            children: [
-              Image.asset(
-                'images/lake.jpg',
-                width: 600,
-                height: 240,
-                fit: BoxFit.cover,
-              ),
-              titleSection,
-              buttonSection,
-              textSection,
-            ],
-          )),
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
         ),
-      ],
+        // Change to buildFoo() for the other examples
+        body: Center(child: buildExpandedImages()),
+      ),
     );
   }
-}
 
-class FavoriteWeget extends StatefulWidget {
-  const FavoriteWeget({super.key});
+  Widget buildOverflowingRow() =>
+      // #docregion overflowing-row
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/pic1.jpg'),
+          Image.asset('images/pic2.jpg'),
+          Image.asset('images/pic3.jpg'),
+        ],
+      );
+  // #enddocregion overflowing-row
 
-  @override
-  State<FavoriteWeget> createState() => _FaboriteWidgetState();
-}
-
-class _FaboriteWidgetState extends State<FavoriteWeget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            onPressed: _toggleFavorite,
-            color: Colors.red,
-            icon: _isFavorited
-                ? const Icon(Icons.star)
-                : const Icon(Icons.star_border),
+  Widget buildExpandedImages() =>
+      // #docregion expanded-images
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Image.asset('images/pic1.jpg'),
           ),
-        ),
-        SizedBox(
-          width: 18,
-          child: SizedBox(
-            child: Text('$_favoriteCount'),
+          Expanded(
+            child: Image.asset('images/pic2.jpg'),
           ),
-        )
-      ],
-    );
-  }
+          Expanded(
+            child: Image.asset('images/pic3.jpg'),
+          ),
+        ],
+      );
+  // #enddocregion expanded-images
+
+  Widget buildExpandedImagesWithFlex() =>
+      // #docregion expanded-images-with-flex
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Image.asset('images/pic1.jpg'),
+          ),
+          Expanded(
+            flex: 2,
+            child: Image.asset('images/pic2.jpg'),
+          ),
+          Expanded(
+            child: Image.asset('images/pic3.jpg'),
+          ),
+        ],
+      );
+  // #enddocregion expanded-images-with-flex
 }
