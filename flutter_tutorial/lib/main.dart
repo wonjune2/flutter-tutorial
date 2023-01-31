00f0170a55b5e4566189ff8739123a88064acdb6
Joon <zzzdnjswns@naver.com>
2023-02-01 00:40:26 +0900

    learning to fly

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,96 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const showCard = false; // Set to false to show Stack
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout demo',
+    return const MaterialApp(
+      title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: Center(child: showCard ? _buildCard() : _buildStack()),
-      ),
-    );
-  }
-
-  // #docregion Card
-  Widget _buildCard() {
-    return SizedBox(
-      height: 210,
-      child: Card(
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text(
-                '1625 Main Street',
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              subtitle: const Text('My City, CA 99984'),
-              leading: Icon(
-                Icons.restaurant_menu,
-                color: Colors.blue[500],
-              ),
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text(
-                '(408) 555-1212',
-                style: TextStyle(fontWeight: FontWeight.w500),
-              ),
-              leading: Icon(
-                Icons.contact_phone,
-                color: Colors.blue[500],
-              ),
-            ),
-            ListTile(
-              title: const Text('costa@example.com'),
-              leading: Icon(
-                Icons.contact_mail,
-                color: Colors.blue[500],
-              ),
-            ),
-          ],
+        body: Center(
+          child: Text('Hello World'),
         ),
       ),
     );
   }
-  // #enddocregion Card
-
-  // #docregion Stack
-  Widget _buildStack() {
-    return Stack(
-      alignment: const Alignment(0.6, 0.6),
-      children: [
-        const CircleAvatar(
-          backgroundImage: AssetImage('images/pic.jpg'),
-          radius: 100,
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.black45,
-          ),
-          child: const Text(
-            'Mia B',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-// #enddocregion Stack
 }
