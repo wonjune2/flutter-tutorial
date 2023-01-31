562c5b72f955d00ad12a25c300077bd248148f95
Joon <zzzdnjswns@naver.com>
2023-01-31 22:31:14 +0900

    lay out info

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = true; // Remove to suppress visual layout
+  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  static const showCard = false; // Set to false to show Stack
 
   @override
   Widget build(BuildContext context) {
@@ -17,58 +19,78 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        // Change to buildFoo() for the other examples
-        body: Center(child: buildExpandedImages()),
+        body: Center(child: showCard ? _buildCard() : _buildStack()),
       ),
     );
   }
 
-  Widget buildOverflowingRow() =>
-      // #docregion overflowing-row
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset('images/pic1.jpg'),
-          Image.asset('images/pic2.jpg'),
-          Image.asset('images/pic3.jpg'),
-        ],
-      );
-  // #enddocregion overflowing-row
+  // #docregion Card
+  Widget _buildCard() {
+    return SizedBox(
+      height: 210,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text(
+                '1625 Main Street',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: const Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text(
+                '(408) 555-1212',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: const Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  // #enddocregion Card
 
-  Widget buildExpandedImages() =>
-      // #docregion expanded-images
-      Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Image.asset('images/pic1.jpg'),
+  // #docregion Stack
+  Widget _buildStack() {
+    return Stack(
+      alignment: const Alignment(0.6, 0.6),
+      children: [
+        const CircleAvatar(
+          backgroundImage: AssetImage('images/pic.jpg'),
+          radius: 100,
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Colors.black45,
           ),
-          Expanded(
-            child: Image.asset('images/pic2.jpg'),
+          child: const Text(
+            'Mia B',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
-          Expanded(
-            child: Image.asset('images/pic3.jpg'),
-          ),
-        ],
-      );
-  // #enddocregion expanded-images
-
-  Widget buildExpandedImagesWithFlex() =>
-      // #docregion expanded-images-with-flex
-      Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Image.asset('images/pic1.jpg'),
-          ),
-          Expanded(
-            flex: 2,
-            child: Image.asset('images/pic2.jpg'),
-          ),
-          Expanded(
-            child: Image.asset('images/pic3.jpg'),
-          ),
-        ],
-      );
-  // #enddocregion expanded-images-with-flex
+        ),
+      ],
+    );
+  }
+// #enddocregion Stack
 }
