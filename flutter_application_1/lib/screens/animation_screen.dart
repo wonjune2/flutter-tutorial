92819b685132b2c6117df8e1dbe0967e29bd56e7
Joon <zzzdnjswns@naver.com>
2023-02-08 22:19:22 +0900

    monitor

--- flutter_application_1/lib/screens/animation_screen.dart
+++ flutter_application_1/lib/screens/animation_screen.dart
@@ -18,7 +18,14 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
       vsync: this,
       duration: Duration(seconds: 2),
     );
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
     controller.forward();
   }
 
